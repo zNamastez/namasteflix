732c8a7ff7865dd5ce3eaf32e54ea04c2799c052
zNamastez <cireso@icloud.com>
2024-10-26 08:39:27 -0300

    Documentação do código

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './Js/App';
 
+// Criação da raiz do React
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Renderiza o aplicativo com StrictMode
 root.render(
   <React.StrictMode>
     <App />
